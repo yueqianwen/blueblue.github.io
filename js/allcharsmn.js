9ac27ae875b059e9b754994818105d32b51f33f1
yueqianwen <37788672+yueqianwen@users.noreply.github.com>
2018-04-08 14:02:03 +0800

    Update allcharsmn.js

--- js/allcharsmn.js
+++ js/allcharsmn.js
@@ -723,7 +723,7 @@ function doInitializeLoadChars(isFromFirstCreate){
 			    		    					//"<td class='content_img td_heightdefault " + en_displaygznr + "' id='en_gznr_img'></td>" +
 			    		    				"</tr>" +
 			    		    				"<tr>" +
-			    		    					"<td rowspan=4 class='title_rule'><span id='uiid_lbl_guizejuli' class='"+ uilangclass +"' uilang>" + getlanguagetext(global_now_uilanguage,'uiid_lbl_guizejuli') + "</span></td>" +
+			    		    					"<td class='title_rule'><span id='uiid_lbl_guizejuli' class='"+ uilangclass +"' uilang>" + getlanguagetext(global_now_uilanguage,'uiid_lbl_guizejuli') + "</span></td>" +
 			    		    					"<td class='td_heightdefault title_mn'>MN</td>" +
 			    		    					"<td class='content_mn mongolnormal'>" + getconvertedtext('mn_rcn',vvv.rulesample) +"</td>" +
 			    		    					//"<td class='content_img td_heightdefault'></td>" +
@@ -873,8 +873,8 @@ function doInitializeLoadChars(isFromFirstCreate){
 			    		    					"<td rowspan=4 class='title_rule'><span id='uiid_lbl_guizejieshi' class='"+ uilangclass +"' uilang>" + getlanguagetext(global_now_uilanguage,'uiid_lbl_guizejieshi') + "</span></td>" +			    		    			 	
 			    		    			    "</tr>" +
 			    		    				"<tr>" +
-			    		    					"<td rowspan=3 class='td_heightdefault title_mn " + mn_displaygzjs + "' id='mn_gzjs_title'>MN</td>" +
-			    		    					"<td rowspan=3 class='content_mn mongolnormal " + mn_displaygzjs + " " + mn_gzjsbgcolor + "' id='mn_gzjs_content'>" + getconvertedtext('mn_rcn',vvv.description_mn_rcn)  + "</td>" +
+			    		    					"<td  class='td_heightdefault title_mn " + mn_displaygzjs + "' id='mn_gzjs_title'>MN</td>" +
+			    		    					"<td class='content_mn mongolnormal " + mn_displaygzjs + " " + mn_gzjsbgcolor + "' id='mn_gzjs_content'>" + getconvertedtext('mn_rcn',vvv.description_mn_rcn)  + "</td>" +
 			    		    					//"<td class='content_img td_heightdefault " + mn_displaygzjs + " " + mn_gzjsbgcolor + "' id='mn_gzjs_img'><img class='img_edit' charindex='" + i + "' postindex='" + ii + "' ruleindex='" + iii + "' rulefiled=1 charcode='" + v.unicode + "' postype=0 ruletype=1 bxno='" + vv.bianxingno + "' ruleno='" + vvv.ruleno + "' src='./image/editit.png'/></td>" +
 			    		    				"</tr>" +
 			    		    				"<tr>" +
@@ -891,8 +891,8 @@ function doInitializeLoadChars(isFromFirstCreate){
 			    		    					"<td rowspan=4 class='title_rule'><span id='uiid_lbl_guizeneirong' class='"+ uilangclass +"' uilang>" + getlanguagetext(global_now_uilanguage,'uiid_lbl_guizeneirong') + "</span></td>" +
 				    		    			"<tr>" +
 			    		    					//"<td rowspan=3 class='title_rule'><span id='uiid_lbl_guizeneirong' class='"+ uilangclass +"' uilang>" + getlanguagetext(global_now_uilanguage,'uiid_lbl_guizeneirong') + "</span></td>" +
-			    		    					"<td rowspan=3 class='td_heightdefault title_mn " + mn_displaygznr + "' id='mn_gznr_title'>MN</td>" +
-			    		    					"<td rowspan=3 class='content_mn mongolnormal " + mn_displaygznr + "' id='mn_gznr_content'>" + getconvertedtext('mn_rcn',vvv.rulevalue) + "</td>" +
+			    		    					"<td  class='td_heightdefault title_mn " + mn_displaygznr + "' id='mn_gznr_title'>MN</td>" +
+			    		    					"<td  class='content_mn mongolnormal " + mn_displaygznr + "' id='mn_gznr_content'>" + getconvertedtext('mn_rcn',vvv.rulevalue) + "</td>" +
 			    		    					//"<td class='content_img td_heightdefault " + mn_displaygznr + "' id='mn_gznr_img'><img class='img_edit' charindex='" + i + "' postindex='" + ii + "' ruleindex='" + iii + "' rulefiled=4 charcode='" + v.unicode + "' postype=0 ruletype=1 bxno='" + vv.bianxingno + "' ruleno='" + vvv.ruleno + "' src='./image/editit.png'/></td>" +
 			    		    				"</tr>" +
 			    		    				"<tr>" +
@@ -906,7 +906,7 @@ function doInitializeLoadChars(isFromFirstCreate){
 			    		    					//"<td class='content_img td_heightdefault " + en_displaygznr + "' id='en_gznr_img'><img class='img_edit' charindex='" + i + "' postindex='" + ii + "' ruleindex='" + iii + "' rulefiled=6 charcode='" + v.unicode + "' postype=0 ruletype=1 bxno='" + vv.bianxingno + "' ruleno='" + vvv.ruleno + "' src='./image/editit.png'/></td>" +
 			    		    				"</tr>" +
 			    		    				"<tr>" +
-			    		    					"<td rowspan=4 class='title_rule'><span id='uiid_lbl_guizejuli' class='"+ uilangclass +"' uilang>" + getlanguagetext(global_now_uilanguage,'uiid_lbl_guizejuli') + "</span></td>" +
+			    		    					"<td class='title_rule'><span id='uiid_lbl_guizejuli' class='"+ uilangclass +"' uilang>" + getlanguagetext(global_now_uilanguage,'uiid_lbl_guizejuli') + "</span></td>" +
 			    		    					"<td class='td_heightdefault title_mn'>MN</td>" +
 			    		    					"<td class='content_mn mongolnormal'>" + getconvertedtext('mn_rcn',vvv.rulesample) +"</td>" +
 			    		    					//"<td class='content_img td_heightdefault'><img class='img_edit' charindex='" + i + "' postindex='" + ii + "' ruleindex='" + iii + "' rulefiled=7 charcode='" + v.unicode + "' postype=0 ruletype=1 bxno='" + vv.bianxingno + "' ruleno='" + vvv.ruleno + "' src='./image/editit.png'/></td>" +
